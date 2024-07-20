89a80ded7ef1e226639e8d51aff084adc7a3be07
程旭东 <xiyang080@126.com>
2024-07-20 11:14:42 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import { size } from 'lodash-es'
+const _ = require('lodash-es')
+
+console.log('size', size([1, 3]))
+console.log('size', _.size([1, 3, 2]))
+console.log('__dirname', __dirname)
+// console.log('import.meta.url', import.meta.url)
