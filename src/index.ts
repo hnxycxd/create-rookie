3312f98fbc3ad0e1e9ed7f1cfa37e8e04393c961
程旭东 <xiyang080@126.com>
2024-07-28 21:56:37 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import url from 'node:url'
+
+const copyRecursion = (templateDir: string, destDir: string) => {
+  const fileList = fs.readdirSync(templateDir, { withFileTypes: true })
+
+  fileList.forEach((file) => {
+    const template = path.join(templateDir, file.name)
+    const dest = path.join(destDir, file.name)
+
+    if (file.isDirectory()) {
+      fs.mkdirSync(dest, { recursive: true })
+      copyRecursion(template, dest)
+    } else {
+      fs.copyFileSync(template, dest)
+    }
+  })
+}
+
+const copy = (templatePath: string, workingPath: string) => {
+  try {
+    const destFolderPath = path.join(workingPath, path.basename(templatePath))
+
+    fs.mkdirSync(destFolderPath, { recursive: true })
+
+    copyRecursion(templatePath, destFolderPath)
+
+    console.log('successfully!')
+  } catch (error) {
+    console.error('Error :', error)
+  }
+}
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const templatePath = path.join(dirname, './template/node-ts')
+const workingPath = process.cwd()
+copy(templatePath, workingPath)
