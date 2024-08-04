794268606c15b679a84b2df3a6b60f23a807e79a
程旭东 <xiyang080@126.com>
2024-08-04 22:40:20 +0800

    init

--- src/template/node-ts/src/index.ts
+++ src/template/node-ts/src/index.ts
@@ -1,8 +1,6 @@
-import path from 'node:path'
+// import path from 'node:path'
 import { size } from 'lodash-es'
-const _ = require('lodash-es')
 
 console.log('size', size([1, 3]))
-console.log('size', _.size([1, 3, 2]))
-console.log('path', path.join(__dirname, 'index.ts'))
-// console.log(import.meta.url)
+// console.log('path', path.join(__dirname, 'index.ts'))
+console.log(import.meta.url)
